a8fbeef69a193c3fcec5e3d0c30998241a91d489
Prabhu Prasad Lenka <prabhuprasadlenka111@gmail.com>
2026-08-20 23:37:52 +0530

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
+ // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/nira-client/',
+
   server: {
     port: 3000,
     open: true
